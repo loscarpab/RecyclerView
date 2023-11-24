ebbb48acae230659f75d9e341ad63177c1de27c1
loscarpab <ccormor392@g.educaand.es>
2023-11-24 19:04:44 +0100

    7 terminado

--- app/src/main/java/com/ccormor392/recyclerview/screens/SuperHero.kt
+++ app/src/main/java/com/ccormor392/recyclerview/screens/SuperHero.kt
@@ -1,14 +1,15 @@
 package com.ccormor392.recyclerview.screens
 
 
+import android.content.Context
+import android.widget.Toast
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
@@ -16,6 +17,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -47,21 +49,24 @@ fun getSuperHeroes():MutableList<Superhero>{
 @Preview
 @Composable
 fun SuperHeroView(){
-    LazyRow(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+    val context = LocalContext.current
+    LazyColumn(verticalArrangement = Arrangement.spacedBy(8.dp)) {
         items(getSuperHeroes()) { superhero ->
-            ItemHero(superhero = superhero)
+            ItemHero(superhero = superhero, onItemSelected = { makeToast(it.realName, context)})
         }
     }
 
 }
 @Composable
-fun ItemHero(superhero: Superhero){
-    Card(border = BorderStroke(2.dp, Color.Red), modifier = Modifier.width(200.dp)) {
+fun ItemHero(superhero: Superhero, onItemSelected:(Superhero)->Unit){
+    Card(border = BorderStroke(2.dp, Color.Red), modifier = Modifier.fillMaxWidth()) {
         Image(
             painter = painterResource(id = superhero.photo),
             contentDescription = "SuperHero Avatar",
             contentScale = ContentScale.Crop,
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable { onItemSelected(superhero) }
         )
         TextoCard(cadenaAEscribir = superhero.superheroName)
         TextoCard(cadenaAEscribir = superhero.realName)
@@ -71,6 +76,12 @@ fun ItemHero(superhero: Superhero){
 @Composable
 fun TextoCard(cadenaAEscribir:String, alineacion:TextAlign = TextAlign.Center, paddingEnd:Int = 0){
     Text(text = cadenaAEscribir,
-        modifier = Modifier.fillMaxWidth(),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(end = paddingEnd.dp),
         textAlign = alineacion)
+}
+
+fun makeToast(textoEnToast:String, context: Context) {
+    Toast.makeText(context, textoEnToast, Toast.LENGTH_SHORT).show()
 }
